aefd28afd83a835f3f2eebd2da56976f85e0954f
mishita-chauhan <mishitachauhan2@gmail.com>
2025-02-22 18:41:18 +0530

    minor changes

--- main.py
+++ main.py
@@ -7,10 +7,5 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/test")
-def read_root():
-    return {"test": "success"}
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
